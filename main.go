3cb72c9077e61d320416ae8e6c2d1b9eca60ddb5
Ryan Carrier <ryan.j.carrier@gmail.com>
2021-11-19 12:02:03 +1000

    updated to properly get wallet balances for tokens

--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-const ethipc = "/eth.ipc"
-
 var (
-	addressList     []Address
+	addressList     []Address = make([]Address, 0)
 	balanceList     []Balance
-	port            string
-	url             string
+	tokenList       []TokenData
+	port            string = "8084"
+	url             string = "http://localhost:8545"
 	lastRefresh     time.Duration
-	eth             *ethclient.Client
+	client          *ethclient.Client
 	refreshDuration time.Duration = time.Second * 15
 )
 
@@ -26,7 +28,7 @@ func init() {
 	var err error
 	portCustom := os.Getenv("PORT")
 	duration := os.Getenv("DURATION")
-	url = os.Getenv("GETH")
+	urlCustom := os.Getenv("GETH")
 	addresses := os.Getenv("ADDRESSES")
 	if len(portCustom) != 0 {
 		port = portCustom
@@ -37,24 +39,39 @@ func init() {
 			panic(err)
 		}
 	}
-	if len(url) == 0 {
-		if _, err = os.Stat(ethipc); err != nil {
-			panic("no geth url supplied (or ipc file in /geth.ipc)")
-		}
-		url = ethipc
+	if len(urlCustom) != 0 {
+		fmt.Println("using custom geth:", urlCustom)
+		url = urlCustom
 	}
 	if len(addresses) == 0 {
 		panic("no addresses supplied")
 	}
-	parseAddresses(addresses)
-}
-
-func init() {
-	var err error
-	eth, err = ethclient.Dial(url)
+	client, err = ethclient.Dial(url)
 	if err != nil {
 		panic(err)
 	}
+	addressList = parseAddresses(addresses)
+}
+
+func init() {
+	tokenlisturl := "https://raw.githubusercontent.com/Uniswap/default-token-list/main/src/tokens/mainnet.json"
+	resp, err := http.Get(tokenlisturl)
+	if err != nil {
+		fmt.Printf("Could not get token list: %s\n", err)
+	}
+	tokenListData, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Println("Err: error reading body of response, ", err)
+	}
+	json.Unmarshal(tokenListData, &tokenList)
+	if err != nil {
+		fmt.Println("Err: Decoding tokenlist, ", err)
+	}
+	for i := range tokenList {
+		tokenList[i].realAddress = common.HexToAddress(tokenList[i].Address)
+	}
+
 }
 
 func main() {
@@ -67,7 +84,7 @@ func main() {
 func handleMetrics(w http.ResponseWriter, r *http.Request) {
 	var resp []string
 	for _, v := range balanceList {
-		resp = append(resp, fmt.Sprintf("crypto_balance{name=\"%v\",address=\"%v\"} %v", v.name, v.address, v.balance))
+		resp = append(resp, fmt.Sprintf("crypto_balance{name=\"%s\",address=\"%s\",symbol=\"%s\"} %v", v.name, v.address, v.symbol, v.balance))
 	}
 	resp = append(resp, fmt.Sprintf("crypto_load_seconds %0.2f", lastRefresh.Seconds()))
 	fmt.Fprintln(w, strings.Join(resp, "\n"))
--- wallets.go
+++ wallets.go
@@ -12,6 +12,15 @@ import (
 	ens "github.com/wealdtech/go-ens/v3"
 )
 
+type TokenData struct {
+	Name        string `json:"name"`
+	Address     string `json:"address"`
+	realAddress common.Address
+	Symbol      string `json:"symbol"`
+	Decimals    uint8  `json:"decimals"`
+	ChainId     uint8  `json:"chainId"`
+	LogoURI     string `json:"logoURI"`
+}
 type Address struct {
 	name    string
 	address common.Address
@@ -19,9 +28,9 @@ type Address struct {
 
 type Balance struct {
 	Address
-	balance      string
-	tokenAddress string
-	symbol       string
+	balance string
+	token   TokenData
+	symbol  string
 }
 
 func walletLoop() {
@@ -31,57 +40,65 @@ func walletLoop() {
 }
 
 func refreshBalances() {
-	fmt.Print("Refreshing...")
+	fmt.Printf("Refreshing %d addresses and %d balances... ", len(addressList), len(balanceList))
 	start := time.Now()
+	refreshAddressBalances()
 	for i, v := range balanceList {
-		balanceList[i].balance = GetBalance(v.address).String()
+		if (v.token == TokenData{}) {
+			balanceList[i].balance = GetBalance(v.address).String()
+		} else {
+			balanceList[i].balance = getTokenBalance(v.token, v.address).String()
+		}
+
 	}
 	lastRefresh = time.Since(start)
-	fmt.Printf(" Completed (%s)", lastRefresh)
+	fmt.Printf(" Completed (%s)\n", lastRefresh)
 }
 
-func getTokenBalance(tokenAddress, address common.Address) *big.Float {
-	caller, err := NewTokenCaller(tokenAddress, eth)
+func getTokenBalance(token TokenData, address common.Address) *big.Float {
+	caller, err := NewTokenCaller(token.realAddress, client)
 	if err != nil {
-		fmt.Println("Err on token address: ", tokenAddress)
+		fmt.Println("Err on token address: ", token.realAddress)
 		return nil
 	}
-	decimals, err := caller.Decimals(nil)
-	if err != nil {
-		fmt.Println("Err on token address: ", tokenAddress)
-		return nil
-	}
+
 	balance, err := caller.BalanceOf(nil, address)
 	if err != nil {
-		fmt.Println("Err on token address: ", tokenAddress)
+		fmt.Println("Err on token address: ", token.realAddress)
 		return nil
 	}
-	return IntToDec(balance, float64(decimals))
+	return IntToDec(balance, token.Decimals)
 }
 
 func refreshAddressBalances() {
 	newBalances := []Balance{}
 	for _, v := range addressList {
 		newBalances = append(newBalances, Balance{Address: v, symbol: "ETH"})
-
+		for _, jv := range tokenList {
+			bal := getTokenBalance(jv, v.address)
+			if bal.Cmp(big.NewFloat(0)) != 0 {
+				newBalances = append(newBalances, Balance{Address: v, token: jv, symbol: jv.Symbol})
+			}
+		}
 	}
 	balanceList = newBalances
 }
 
 func GetBalance(address common.Address) *big.Float {
-	balance, err := eth.BalanceAt(context.Background(), address, nil)
+	balance, err := client.BalanceAt(context.Background(), address, nil)
 	if err != nil {
 		fmt.Printf("Error fetching balance (%v)\n", address)
 	}
 	return WeiToEther(balance)
 }
 
-func IntToDec(u *big.Int, decimal float64) *big.Float {
-	return new(big.Float).Quo(new(big.Float).SetInt(u), big.NewFloat(decimal))
+func IntToDec(u *big.Int, decimal uint8) *big.Float {
+	return new(big.Float).Quo(new(big.Float).SetInt(u),
+		new(big.Float).SetInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(decimal)), nil)))
 }
 
 func WeiToEther(wei *big.Int) *big.Float {
-	return IntToDec(wei, params.Ether)
+	return new(big.Float).Quo(new(big.Float).SetInt(wei), big.NewFloat(params.Ether))
 }
 
 func parseAddresses(a string) []Address {
@@ -93,20 +110,23 @@ func parseAddresses(a string) []Address {
 	for _, v := range strings.Split(a, ",") {
 		if common.IsHexAddress(v) {
 			address = common.HexToAddress(v)
-			name, err = ens.ReverseResolve(eth, address)
+			name, err = ens.ReverseResolve(client, address)
 			if err == nil {
 				fmt.Printf("Found ENS (%s) for address (%s)\n", name, address)
 			} else {
 				name = v
 			}
 		} else {
+			fmt.Printf("'%s' does not appear to be hex address attempting to resolve...\n", v)
 			name = v
-			address, err := ens.Resolve(eth, v)
-			//this might be weird cause many address potentially
+			address, err = ens.Resolve(client, v)
+			//this might be weird cause many address potentially? for doge btc etc
 			if err != nil {
-				fmt.Printf("ERR: Address (%s) not a hex address or ENS domain\n", address)
+				fmt.Println("ERROR: getting from ENS", err.Error())
+				fmt.Printf("ERR: Address (%s) not a hex address or ENS domain\n", v)
 				continue
 			}
+			fmt.Printf("Name (%s) successfully resolved to address (%s)\n", v, address)
 		}
 		addresses = append(addresses, Address{name: name, address: address})
 	}
